65d8ba127fc7d237baf7a8914df8f56764044be4
julianschmocker <julian.schmocker@gmail.com>
2023-02-23 18:35:57 +0100

    add draft

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,74 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+library(shiny)
+library(purrr)
+library(dplyr)
+library(purrrlyr)
+library(RSQLite)
+library(DBI)
+# # Create an ephemeral in-memory RSQLite database
+con <- dbConnect(RSQLite::SQLite(), "message")
+df <- data.frame(rowid = numeric(),
+                 user = character(),
+                 text = character(),
+                 time = double())
+dbWriteTable(con, "me", df, overwrite = TRUE)
+
+
+render_msg_divs <- function(texts, users) {
+  purrr::map2(texts, users, ~ div(class="chatMessage",
+                                  p(tags$strong(.y),
+                                  .x))
+  )
 }
+
+ui <- fluidPage(
+  includeCSS(system.file("assets/shinyChatR.css", package = "shinyChatR")),
+  fluidRow(
+    column(width = 6,
+           div(class = "chatContainer",
+               div(class = "chatMessages", style = paste0("height:", "300px;"),
+                   # Display messages here
+                   uiOutput("chatbox")
+               ),
+               tags$form(class = "chatForm",
+                         tags$input(type = "text", id = "chatInput",
+                                    placeholder = "Enter message"),
+                         actionButton("chatFromSend", "Send",
+                                      style = "background-color: #007bc2;
+                                               color: #fff;")
+
+               )
+           )
+    )
+  )
+)
+
+
+
+server <- function(input, output, server) {
+  # Add code for sending and receiving messages here
+  global_rv <- reactiveValues(chat = dbGetQuery(con, 'SELECT * FROM me'))
+
+  output$chatbox <- renderUI({
+    if (!is_empty(global_rv$chat)) {
+      render_msg_divs(global_rv$chat$text, global_rv$chat$user)
+    } else {
+      tags$span("Empty chat")
+    }
+  })
+
+  observeEvent(input$chatFromSend, {
+    dbExecute(con, 'INSERT INTO me (user, text, time)
+                    VALUES (?, ?, ?);', list("User1",
+                                             input$chatInput,
+                                             Sys.time()))
+    global_rv$chat <- dbGetQuery(con, 'SELECT * FROM me')
+  })
+
+}
+
+
+
+
+# Run the application
+shinyApp(ui = ui, server = server)
